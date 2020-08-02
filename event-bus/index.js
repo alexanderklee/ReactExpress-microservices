d1293949ae554d0609aac48673d28464c9a1c2df
Alexander Lee <alexander.k.lee@gmail.com>
2020-08-02 12:26:57 -0700

    adding moderation service

--- event-bus/index.js
+++ event-bus/index.js
@@ -6,14 +6,15 @@ const app = express();
 app.use(bodyParser.json());
 
 app.post('/events', (req,res) => {
+    console.log('Event Received:', req.body.type);
     const event = req.body;
 
     axios.post('http://localhost:4000/events', event);
     axios.post('http://localhost:4001/events', event);
     axios.post('http://localhost:4002/events', event);
+    axios.post('http://localhost:4003/events', event);
 
     res.send({ status: 'OK' });
-
 
 });
 
